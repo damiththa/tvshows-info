c0947899fd845096d5510b9a5a43238d2f725335
Madushan D. Memmendarachchi <damiththa@gmail.com>
2020-07-27 17:06:25 -0400

    Getting interested attributes

--- endpoints/getTVShowsInfo.py
+++ endpoints/getTVShowsInfo.py
@@ -8,13 +8,26 @@ TMDB_API_KEY = os.environ['TMDB_API_KEY']
 tmdb.API_KEY = TMDB_API_KEY
 
 def handler(event, context):
-    
-    movie = tmdb.Movies(603)
-    res = movie.info()
 
-    print (movie.title)
+    # HARDCODED: 
+    # showID = 65798 # LetterKenny
+    showID = 19885 # Sherlock
 
 
+    tv_show = tmdb.TV(showID)
+    res = tv_show.info()
+
+    # interested attributes
+    show_name = tv_show.name
+    show_overview = tv_show.overview
+    show_status = tv_show.status
+    number_of_episodes = tv_show.number_of_episodes
+    number_of_seasons = tv_show.number_of_seasons
+    popularity = tv_show.popularity
+    user_score = tv_show.vote_average
+    show_genres = tv_show.genres # returns a list of dicts. 
+
+    print (popularity, user_score)
 
 
     # TODO: make this more meaningful
