aabc2ce7f1d8d02ca267403300b25f1bb7c58904
Madushan D. Memmendarachchi <damiththa@gmail.com>
2020-07-27 16:06:08 -0400

    Environment variable setup
    Setting up environment variable for both Serverless and for the scrupt

--- endpoints/getTVShowsInfo.py
+++ endpoints/getTVShowsInfo.py
@@ -1,7 +1,24 @@
 import json
 import requests
+import os
+
+import tmdbsimple as tmdb
+
+TMDB_API_KEY = os.environ['TMDB_API_KEY']
+tmdb.API_KEY = TMDB_API_KEY
 
 def handler(event, context):
+    
+    movie = tmdb.Movies(603)
+    res = movie.info()
+
+    print (movie.title)
+
+
+
+
+    # TODO: make this more meaningful
+    # This is for aws lambda return
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
