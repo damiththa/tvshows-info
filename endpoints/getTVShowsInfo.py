49767640a10c7b5fbeb1c194b50873ba38eba052
Madushan D. Memmendarachchi <damiththa@gmail.com>
2020-07-27 15:32:35 -0400

    Project initial commit

---
+++ endpoints/getTVShowsInfo.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+def handler(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
